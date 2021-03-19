7ab8f816f0d0d201de54eb651b51aab14612150e
Vic Shóstak <vikkyshostak@gmail.com>
2021-03-19 19:04:22 +0300

    Add GetBooksByAuthor method

--- app/queries/book_query.go
+++ app/queries/book_query.go
@@ -11,72 +11,93 @@ type BookQueries struct {
 	*sqlx.DB
 }
 
-// GetBooks func for getting all books.
+// GetBooks method for getting all books.
 func (q *BookQueries) GetBooks() ([]models.Book, error) {
 	// Define books variable.
-	var books []models.Book
+	books := []models.Book{}
+
+	// Define query string.
+	query := `SELECT * FROM books`
 
 	// Send query to database.
-	if err := q.Select(&books, `SELECT * FROM books`); err != nil {
-		return []models.Book{}, err
+	err := q.Get(&books, query)
+	if err != nil {
+		return books, err
 	}
 
 	return books, nil
 }
 
-// GetBook func for getting one book by given ID.
-func (q *BookQueries) GetBook(id uuid.UUID) (models.Book, error) {
-	// Define book variable.
-	var book models.Book
+// GetBooksByAuthor method for getting all books by given author.
+func (q *BookQueries) GetBooksByAuthor(author string) ([]models.Book, error) {
+	// Define books variable.
+	books := []models.Book{}
+
+	// Define query string.
+	query := `SELECT * FROM books WHERE author = $1`
 
 	// Send query to database.
-	if err := q.Get(&book, `SELECT * FROM books WHERE id = $1`, id); err != nil {
-		return models.Book{}, err
+	err := q.Get(&books, query, author)
+	if err != nil {
+		return books, err
+	}
+
+	return books, nil
+}
+
+// GetBook method for getting one book by given ID.
+func (q *BookQueries) GetBook(id uuid.UUID) (models.Book, error) {
+	// Define book variable.
+	book := models.Book{}
+
+	// Define query string.
+	query := `SELECT * FROM books WHERE id = $1`
+
+	// Send query to database.
+	err := q.Get(&book, query, id)
+	if err != nil {
+		return book, err
 	}
 
 	return book, nil
 }
 
-// CreateBook func for creating book by given Book object.
+// CreateBook method for creating book by given Book object.
 func (q *BookQueries) CreateBook(u *models.Book) error {
+	// Define query string.
+	query := `INSERT INTO books VALUES ($1, $2, $3, $4, $5, $6, $7)`
+
 	// Send query to database.
-	if _, err := q.Exec(
-		`INSERT INTO books VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-		u.ID,
-		u.CreatedAt,
-		u.UpdatedAt,
-		u.Title,
-		u.Author,
-		u.BookStatus,
-		u.BookAttrs,
-	); err != nil {
+	_, err := q.Exec(query, u.ID, u.CreatedAt, u.UpdatedAt, u.Title, u.Author, u.BookStatus, u.BookAttrs)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// UpdateBook func for updating book by given Book object.
+// UpdateBook method for updating book by given Book object.
 func (q *BookQueries) UpdateBook(u *models.Book) error {
+	// Define query string.
+	query := `UPDATE books SET updated_at = $2, title = $3, author = $4, book_attrs = $5 WHERE id = $1`
+
 	// Send query to database.
-	if _, err := q.Exec(
-		`UPDATE books SET updated_at = $2, title = $3, author = $4, book_attrs = $5 WHERE id = $1`,
-		u.ID,
-		u.UpdatedAt,
-		u.Title,
-		u.Author,
-		u.BookAttrs,
-	); err != nil {
+	_, err := q.Exec(query, u.ID, u.UpdatedAt, u.Title, u.Author, u.BookAttrs)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// DeleteBook func for delete book by given ID.
+// DeleteBook method for delete book by given ID.
 func (q *BookQueries) DeleteBook(id uuid.UUID) error {
+	// Define query string.
+	query := `DELETE FROM books WHERE id = $1`
+
 	// Send query to database.
-	if _, err := q.Exec(`DELETE FROM books WHERE id = $1`, id); err != nil {
+	_, err := q.Exec(query, id)
+	if err != nil {
 		return err
 	}
 
